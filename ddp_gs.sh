0a2f84492ab193c3e1dfd1ba5d72f25020b8fa11
Whishing <1064432511@qq.com>
2025-09-07 19:52:03 +0800

    modified:   ddp_gs.sh 	modified:   test.sh

--- ddp_gs.sh
+++ ddp_gs.sh
@@ -1,11 +1,6 @@
 
-# CUDA_VISIBLE_DEVICES=7,8,9 torchrun --nproc_per_node=3 --master_port=29519 train.py --model_save_path /media/HDD2/models/finals/arkit \
-#     --scene_model_dir /media/HDD2/ARKitScenes/ARKitScenes_3dgs/train \
-#     --scene_source_dir /media/HDD2/ARKitScenes/ARKitScenes_nerf/train \
-#     --iterations 300000
-
-CUDA_VISIBLE_DEVICES=7,8,9 torchrun --nproc_per_node=3 --master_port=29519 train.py --model_save_path /media/HDD2/shenxing/dpt_models/release_debug/scans_gs \
-    --scene_model_dir /media/SSD/shenxing/datasets/Scannetpp/Scannetpp_3dgs/train /media/SSD/shenxing/datasets/scannet_3dgs/train\
-    --scene_source_dir /media/SSD/shenxing/datasets/Scannetpp/Scannetpp_nerf/train /media/SSD/shenxing/datasets/scannet_nerf/scans\
+CUDA_VISIBLE_DEVICES=7,8,9 torchrun --nproc_per_node=3 --master_port=29519 train.py --model_save_path /media/HDD2/models/finals/arkit \
+    --scene_model_dir /media/HDD2/ARKitScenes/ARKitScenes_3dgs/train \
+    --scene_source_dir /media/HDD2/ARKitScenes/ARKitScenes_nerf/train \
     --iterations 300000
 
--- test.sh
+++ test.sh
@@ -1,23 +1,18 @@
 
-# CUDA_VISIBLE_DEVICES=$1 python test_pointcloud.py --dpt_model_path /media/HDD2/models/finals/scans/dpt_ckpt_0150000.pth \
-#     --scene_model_dir  /media/SSD/sparse_pts/Scannetpp/test/ /media/SSD/sparse_pts/scannet/test/  /media/SSD/sparse_pts/ARKitScenes/test/\
-#                         /media/SSD/sparse_pts/Multiscan/test/ /media/SSD/sparse_pts/Multiscan/train/ \
-#     --scene_source_dir /media/HDD2/Scannetpp/Scannetpp_nerf/test /media/HDD2/scannet_nerf/scans_test /media/HDD2/ARKitScenes/ARKitScenes_nerf/test/ \
-#                         /media/HDD2/Multiscan/Multiscan_nerf/test /media/HDD2/Multiscan/Multiscan_nerf/train \
+CUDA_VISIBLE_DEVICES=$1 python test_pointcloud.py --dpt_model_path /media/HDD2/models/finals/scans/dpt_ckpt_0150000.pth \
+    --scene_model_dir  /media/SSD/sparse_pts/Scannetpp/test/ /media/SSD/sparse_pts/scannet/test/  /media/SSD/sparse_pts/ARKitScenes/test/\
+                        /media/SSD/sparse_pts/Multiscan/test/ /media/SSD/sparse_pts/Multiscan/train/ \
+    --scene_source_dir /media/HDD2/Scannetpp/Scannetpp_nerf/test /media/HDD2/scannet_nerf/scans_test /media/HDD2/ARKitScenes/ARKitScenes_nerf/test/ \
+                        /media/HDD2/Multiscan/Multiscan_nerf/test /media/HDD2/Multiscan/Multiscan_nerf/train \
 
 
-# CUDA_VISIBLE_DEVICES=$1 python test.py --dpt_model_path /media/HDD2/models/finals/scan_scanpp/dpt_ckpt_0100000.pth \
-#     --scene_model_dir  /media/HDD2/Scannetpp/Scannetpp_3dgs/test/ /media/HDD2/scannet_3dgs/test/ /media/HDD2/ARKitScenes/ARKitScenes_3dgs/test/\
-#                         /media/HDD2/Multiscan/Multiscan_3dgs/test /media/HDD2/Multiscan/Multiscan_3dgs/train\
-#     --scene_source_dir /media/HDD2/Scannetpp/Scannetpp_nerf/test /media/HDD2/scannet_nerf/scans_test /media/HDD2/ARKitScenes/ARKitScenes_nerf/test/ \
-#                         /media/HDD2/Multiscan/Multiscan_nerf/test /media/HDD2/Multiscan/Multiscan_nerf/train\
+CUDA_VISIBLE_DEVICES=$1 python test.py --dpt_model_path /media/HDD2/models/finals/scan_scanpp/dpt_ckpt_0100000.pth \
+    --scene_model_dir  /media/HDD2/Scannetpp/Scannetpp_3dgs/test/ /media/HDD2/scannet_3dgs/test/ /media/HDD2/ARKitScenes/ARKitScenes_3dgs/test/\
+                        /media/HDD2/Multiscan/Multiscan_3dgs/test /media/HDD2/Multiscan/Multiscan_3dgs/train\
+    --scene_source_dir /media/HDD2/Scannetpp/Scannetpp_nerf/test /media/HDD2/scannet_nerf/scans_test /media/HDD2/ARKitScenes/ARKitScenes_nerf/test/ \
+                        /media/HDD2/Multiscan/Multiscan_nerf/test /media/HDD2/Multiscan/Multiscan_nerf/train\
 
 
 
-CUDA_VISIBLE_DEVICES=$1 python test.py --dpt_model_path /media/HDD2/shenxing/dpt_models/release_debug/scans_gs/dpt_ckpt_0080000.pth \
-    --scene_model_dir  /media/HDD2/shenxing/Scannetpp/Scannetpp_3dgs/test/ /media/HDD2/shenxing/scannet_3dgs/test/ /media/HDD2/shenxing/ARKitScenes/ARKitScenes_3dgs/test/\
-                        /media/HDD2/shenxing/Multiscan/Multiscan_3dgs/test /media/HDD2/shenxing/Multiscan/Multiscan_3dgs/train\
-    --scene_source_dir /media/HDD2/shenxing/Scannetpp/Scannetpp_nerf/test /media/HDD2/shenxing/scannet_nerf/scans_test /media/HDD2/shenxing/ARKitScenes/ARKitScenes_nerf/test/ \
-                        /media/HDD2/shenxing/Multiscan/Multiscan_nerf/test /media/HDD2/shenxing/Multiscan/Multiscan_nerf/train\
 
 
